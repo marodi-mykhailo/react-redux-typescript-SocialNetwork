a0c93bb3fa8d95ca2da7f34136d3d4281b904573
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-08-13 17:57:36 +0200

    Initial commit, create few components and add a bit styles

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './Content.module.css';
+const Content = () =>{
+    return(
+        <div className={styles.contentWrapper}>
+            content
+        </div>
+    )
+}
+
+export default Content;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Header.module.css';
+
+const Head = () => {
+    return(
+        <header className={styles.header}>
+            hejka
+        </header>
+    )
+}
+
+export default Head;
