743b1b868a7afe9883f251c8ce8e2349a7c654b3
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-08-16 17:45:52 +0200

    refactoring code

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,25 +3,34 @@ import style from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./DialogItem/Message/Message";
 
-type DialogsPropsType = {
-
-}
-
+type DialogsPropsType = {}
 
 
 const Dialogs = (props: DialogsPropsType) => {
+    const dialogs = [
+        {id: '1', name: 'Misha'},
+        {id: '2', name: 'Yura'},
+        {id: '3', name: 'Nastya'},
+        {id: '4', name: 'Ivan'},
+    ]
+
+    let dialogsElements = dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>)
+
+    const messages = [
+        {id: '1', message: 'Hi'},
+        {id: '2', message: 'How is your day?'},
+        {id: '3', message: 'Bue have a good day'},
+    ]
+
+    let messagesElements = messages.map(message => <Message message={message.message}/>)
+
     return (
         <div className={style.dialogs}>
             <div className={style.dialogsItems}>
-                <DialogItem name={"Misha"} id={'1'}/>
-                <DialogItem name={"Yura"} id={'2'}/>
-                <DialogItem name={"Nastya"} id={'3'}/>
-                <DialogItem name={"Ivan"} id={'4'}/>
+                {dialogsElements}
             </div>
             <div className={style.messages}>
-                <Message message={"Hi"}/>
-                <Message message={"How is your day?"}/>
-                <Message message={"Bue hawe a good day"}/>
+                {messagesElements}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,14 @@ import styles from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    const post = [
+        {id: '1', message: 'Hi, how are you?', likesCount: 5},
+        {id: '2', message: 'I love React.js', likesCount: 300},
+    ]
+
+    let postsElements = post.map(post => <Post message={post.message} likesCount={post.likesCount}/>)
+
     return (
         <div className={styles.postsBlock}>
             <h3>My Post</h3>
@@ -14,8 +22,7 @@ const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={styles.posts}>
-                <Post message={'Hi, how are you?'} likesCount={5}/>
-                <Post message={'I love React.js'} likesCount={300}/>
+                {postsElements}
             </div>
         </div>
     )
